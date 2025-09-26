29bdf41c1cc928c79a0d74c96d71e2adf08acc68
Feras Khalil <x.feraskhalil@gmail.com>
2025-09-26 19:42:24 +0300

    Change hero image to smaller one

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useRef } from "react";
 import Header from "./Header";
-import laptop_device from "../assets/laptop_device.png"
+import laptop_device from "../assets/laptop_device_2.png"
 interface HeroSectionProps {
   className?: string;
 }
